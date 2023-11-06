2216e3c59f3bea9e206eecf427dba080493aa142
Crazygriferman <251031557@qq.com>
2023-11-06 12:26:55 -0500

    add: les miserables

--- scripts/main.js
+++ scripts/main.js
@@ -2,33 +2,70 @@
 // let myChart = echarts.init(chartDom);
 const myChart = echarts.init(document.querySelector('#main'))
 let option;
-fetch("/assets/data.json")
+fetch("/assets/test.json")
   .then(response => response.json())
   .then(json => {
+    json.nodes.forEach(function (node) {
+      node.label = {
+        // show: node.symbolSize > 30
+        show: true
+      }
+    });
     option = {
-      tooltip: {
-        trigger: 'item',
-        triggerOn: 'mousemove'
+      title: {
+        text: 'Les Miserables',
+        subtext: 'Circular layout',
+        top: 'bottom',
+        left: 'right'
       },
+      tooltip: {},
+      legend: [
+        {
+          data: json.categories.map(function (a) {
+            return a.name;
+          }),
+          emphasis: {
+            show: true
+          }
+        }
+      ],
+      animationDurationUpdate: 1500,
+      animationEasingUpdate: 'quinticInOut',
       series: [
         {
-          type: 'tree',
-          data: [json],
-          top: '18%',
-          bottom: '14%',
-          layout: 'radial',
-          symbol: 'emptyCircle',
-          symbolSize: 7,
-          initialTreeDepth: 3,
-          animationDurationUpdate: 750,
+          name: 'Les Miserables',
+          type: 'graph',
+          layout: 'circular',
+          circular: {
+            rotateLabel: true
+          },
+          symbol: 'pin',
+          symbolSize: 200,
+          data: json.nodes,
+          links: json.links,
+          categories: json.categories,
+          roam: true,
+          label: {
+            position: 'right',
+            formatter: '{b}'
+          },
+          lineStyle: {
+            color: 'source',
+            curveness: 0.3
+          },
           emphasis: {
-            focus: 'descendant'
+            focus: 'adjacency',
+            lineStyle: {
+              width: 10
+            }
           }
         }
       ]
     };
   option && myChart.setOption(option);
-    
+  myChart.on("click", (res) => {
+    console.log(res);
+  })
   });
 
 
